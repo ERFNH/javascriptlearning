6a204876fc8c38390b4ec9f73cfce3d892b04d7f
ERFNH <127482284+ERFNH@users.noreply.github.com>
2025-09-18 15:43:46 -0700

    bar prj

---
+++ bar/sidebar/README.md
@@ -0,0 +1,3 @@
+# Sidebar Menu
+
+A simple responsive sidebar menu with open/close buttons built with HTML, CSS, and JavaScript.
---
+++ bar/sidebar/sidebar.js
@@ -0,0 +1,12 @@
+const togglebtn = document.querySelector(".sidebar-toggle");
+const closebtn = document.querySelector(".close");
+const sidebar = document.querySelector(".sidebar");
+
+togglebtn.addEventListener("click", function () {
+  sidebar.classList.toggle("show-sidebar");
+});
+
+closebtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
+
